1cdc7fe35dc851304bc30205e2d880d3308aaac7
Eichi2514 <picon1317@gmail.com>
2024-07-09 10:30:12 +0900

    finish

--- src/main/java/org/koreait/CurrentDateTime.java
+++ src/main/java/org/koreait/CurrentDateTime.java
@@ -5,11 +5,8 @@ import java.time.format.DateTimeFormatter;
 
 public class CurrentDateTime {
     public static String Now() {
-        // 현재 날짜/시간
         LocalDateTime now = LocalDateTime.now();
-        // 포맷팅
         String formatedNow = now.format(DateTimeFormatter.ofPattern("yy-MM-dd HH:mm:ss"));
-        // 포맷팅 현재 날짜/시간 출력
-        return formatedNow;  // 2021년 06월 17일 06시 43분 21초     }}
+        return formatedNow;
     }
 }
