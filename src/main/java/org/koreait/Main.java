eaade39c45d060ca8b745eb6606637fe78aa917e
Eichi2514 <picon1317@gmail.com>
2024-07-09 10:29:03 +0900

    진짜완료

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("== 명언 앱 실행 ==");
         List<Saying> sayings = new ArrayList<>();
         Scanner in = new Scanner(System.in);
         int listNumber = 1;
